0c8c63b5271e5743d0c035f7238382c1785984bd
elpidaguy <exabytes.js@gmail.com>
2019-04-01 20:36:24 +0530

    complete functionality changes

--- app.js
+++ app.js
@@ -1,238 +1,5 @@
 var bidApp = angular.module('bidApp', ['ngRoute']);
 
-bidApp.directive('fileModel', ['$parse', function($parse) {
- return {
-  restrict: 'A',
-  link: function(scope, element, attrs) {
-   var model = $parse(attrs.fileModel);
-   var modelSetter = model.assign;
-
-   element.bind('change', function() {
-    scope.$apply(function() {
-      console.log(attrs.fileModel);
-
-     modelSetter(scope, element[0].files[0]);
-     /*if (attrs.fileModel == "amcExcelFile") {
-      scope.uploadAmcExcel();    
-     }*/
-    });
-   });
-  }
- };
-}]);
-
-bidApp.directive('stSelectDistinct', [function() {
-      return {
-        restrict: 'E',
-        require: '^stTable',
-        scope: {
-          collection: '=',
-          predicate: '@',
-          predicateExpression: '='
-        },
-        template: '<select ng-model="selectedOption" ng-change="optionChanged(selectedOption)" ng-options="opt for opt in distinctItems"></select>',
-        link: function(scope, element, attr, table) {
-          var getPredicate = function() {
-            var predicate = scope.predicate;
-            if (!predicate && scope.predicateExpression) {
-              predicate = scope.predicateExpression;
-            }
-            return predicate;
-          }
-
-          scope.$watch('collection', function(newValue) {
-            var predicate = getPredicate();
-
-            if (newValue) {
-              var temp = [];
-              scope.distinctItems = ['All'];
-
-              angular.forEach(scope.collection, function(item) {
-
-                var value = item[predicate].toString();                
-
-                if (value && value.trim().length > 0 && temp.indexOf(value) === -1) {
-                  temp.push(value.toString());
-                }
-              });
-              temp.sort();
-
-              scope.distinctItems = scope.distinctItems.concat(temp);
-              scope.selectedOption = scope.distinctItems[0];
-              scope.optionChanged(scope.selectedOption);
-            }
-          }, true);
-
-          scope.optionChanged = function(selectedOption) {
-            var predicate = getPredicate();
-
-            var query = {};
-
-            query.distinct = selectedOption;
-
-            if (query.distinct === 'All') {
-              query.distinct = '';
-            }
-
-            table.search(query, predicate);
-          };
-        }
-      }
-    }]);
-    
-
-
-
-bidApp.filter('unique', function() {
-    return function (arr, field) {
-        console.log(field);
-        var o = {}, i, l = arr.length, r = [];
-        for(i=0; i<l;i+=1) {
-            o[arr[i][field]] = arr[i];
-        }
-        for(i in o) {
-            r.push(o[i]);
-        }
-
-/*        console.log(r);
-*/        return r;
-    };
-  });
-
-
-bidApp.filter('customFilter', ['$filter', function($filter) {
-      var filterFilter = $filter('filter');
-      var standardComparator = function standardComparator(obj, text) {
-        text = ('' + text).toLowerCase();
-        return ('' + obj).toLowerCase().indexOf(text) > -1;
-      };
-
-      return function customFilter(array, expression) {
-        function customComparator(actual, expected) {
-
-          var isBeforeActivated = expected.before;
-          var isAfterActivated = expected.after;
-          var isLower = expected.lower;
-          var isHigher = expected.higher;
-          var higherLimit;
-          var lowerLimit;
-          var itemDate;
-          var queryDate;
-
-          if (angular.isObject(expected)) {
-            //exact match
-            if (expected.distinct) {
-              if (!actual || actual.toLowerCase() !== expected.distinct.toLowerCase()) {
-                return false;
-              }
-
-              return true;
-            }
-
-            //matchAny
-            if (expected.matchAny) {
-              if (expected.matchAny.all) {
-                return true;
-              }
-
-              if (!actual) {
-                return false;
-              }
-
-              for (var i = 0; i < expected.matchAny.items.length; i++) {
-                if (actual.toLowerCase() === expected.matchAny.items[i].toLowerCase()) {
-                  return true;
-                }
-              }
-
-              return false;
-            }
-
-            //date range
-            if (expected.before || expected.after) {
-              try {
-                if (isBeforeActivated) {
-                  higherLimit = expected.before;
-
-                  itemDate = new Date(actual);
-                  queryDate = new Date(higherLimit);
-
-                  if (itemDate > queryDate) {
-                    return false;
-                  }
-                }
-
-                if (isAfterActivated) {
-                  lowerLimit = expected.after;
-
-
-                  itemDate = new Date(actual);
-                  queryDate = new Date(lowerLimit);
-
-                  if (itemDate < queryDate) {
-                    return false;
-                  }
-                }
-
-                return true;
-              } catch (e) {
-                return false;
-              }
-
-            } else if (isLower || isHigher) {
-              //number range
-              if (isLower) {
-                higherLimit = expected.lower;
-
-                if (actual > higherLimit) {
-                  return false;
-                }
-              }
-
-              if (isHigher) {
-                lowerLimit = expected.higher;
-                if (actual < lowerLimit) {
-                  return false;
-                }
-              }
-
-              return true;
-            }
-            //etc
-
-            return true;
-
-          }
-          return standardComparator(actual, expected);
-        }
-
-        var output = filterFilter(array, expression, customComparator);
-        return output;
-      };
-    }]);
-
-bidApp.filter('titlecase', function() {
-     return function(input) {
-      var smallWords = /^(a|an|and|as|at|but|by|en|for|if|in|nor|of|on|or|per|the|to|vs?\.?|via)$/i;
-
-      input = input.toLowerCase();
-      return input.replace(/[A-Za-z0-9\u00C0-\u00FF]+[^\s-]*/g, function(match, index, title) {
-       if (index > 0 && index + match.length !== title.length &&
-        match.search(smallWords) > -1 && title.charAt(index - 2) !== ":" &&
-        (title.charAt(index + match.length) !== '-' || title.charAt(index - 1) === '-') &&
-        title.charAt(index - 1).search(/[^\s-]/) < 0) {
-        return match.toLowerCase();
-       }
-
-       if (match.substr(1).search(/[A-Z]|\../) > -1) {
-        return match;
-       }
-
-       return match.charAt(0).toUpperCase() + match.substr(1);
-      });
-     }
-    });
-
     bidApp.config(function($sceProvider) {
         $sceProvider.enabled(false);
     });
@@ -273,7 +40,6 @@ bidApp.controller('homeController', function($scope,$http,$location)
 {
     console.log("in homeController");
     $scope.userdata = JSON.parse(localStorage.userdata);
-
     
     $scope.answers = [];
     $scope.questions = [];
@@ -408,6 +174,62 @@ bidApp.controller('homeController', function($scope,$http,$location)
             iziToast.error({title: 'Error',message: "Server Error !", position: 'topRight'});
         });
     };
+
+$scope.notice = {};
+    $scope.addNotice = function() {
+      // console.log($scope.notice)
+      if(($scope.notice.title == undefined || !$scope.notice.description == undefined))
+      {
+        iziToast.error({title: 'Error',message: "Please enter all fields!", position: 'topRight'});
+        return;
+      }
+
+      $scope.userdata = JSON.parse(localStorage.userdata);
+      console.log($scope.notice);
+      $scope.notice['noitceBy'] = $scope.userdata['fname'] + $scope.userdata['lname'];
+      $scope.notice['noticeDate'] = new Date();
+      var param = JSON.stringify(
+        $scope.notice
+      );
+
+      console.log(param);
+
+        $http.post("http://localhost:6969/addNotice",param)
+        .success(function (data) {
+            // console.log(data);
+            if (data.success == "true") {
+              console.log('success');
+                iziToast.success({theme: 'dark',title:'Success',message: data.message,position: 'topRight',icon: 'fa fa-user',progressBarColor: 'rgb(0, 255, 184)'});
+                location.reload();
+                // $location.path('login');
+              } else {
+                iziToast.error({title: 'Error',message: data.message, position: 'topRight'});
+            }
+        })
+        .error(function (err) {
+            iziToast.error({title: 'Error',message: "Server Error !", position: 'topRight'});
+        });
+    }
+
+$scope.notices = [];
+    $scope.getNotices = function() {
+      $http.get("http://localhost:6969/getNotices")
+        .success(function (data) {
+            // console.log(data);
+            if (data.success == "true") {
+              console.log('success');
+              $scope.notices = data.notices;
+                // iziToast.success({theme: 'dark',title:'Success',message: data.message,position: 'topRight',icon: 'fa fa-user',progressBarColor: 'rgb(0, 255, 184)'});
+                // location.reload();
+                // $location.path('login');
+              } else {
+                iziToast.error({title: 'Error',message: data.message, position: 'topRight'});
+            }
+        })
+        .error(function (err) {
+            iziToast.error({title: 'Error',message: "Server Error !", position: 'topRight'});
+        });
+    }
 });
 
 
